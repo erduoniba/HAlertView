b2b178b841c62de9ddaf10c71f8e95d9bc4de230
Harry <328418417@qq.com>
2014-10-16 13:18:35 +0800

    Initial Commit

---
+++ HarryAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HarryAlertView
+//
+//  Created by Harry on 14-10-16.
+//  Copyright (c) 2014年 Harry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HarryAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HarryAlertView
+//
+//  Created by Harry on 14-10-16.
+//  Copyright (c) 2014年 Harry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
